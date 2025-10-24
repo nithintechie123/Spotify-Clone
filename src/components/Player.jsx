08c29877b2c6a4f30797ec4e853f2192de990141
Nithin Kumar Gorintala <121857100+nithintechie123@users.noreply.github.com>
2025-10-24 21:31:16 +0530

    final commit

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 import { PlayerContext } from '../context/PlayerContext';
 
 const Player = () => {
-    const {seekBar,seekBg,playStatus,play,pause,track,time}=useContext(PlayerContext);
+    const {seekBar,seekBg,playStatus,play,pause,track,time,previous,next,seekSong}=useContext(PlayerContext);
 
   return (
     <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
@@ -17,17 +17,17 @@ const Player = () => {
         <div className="flex flex-col items-center gap-1 m-auto">
             <div className='flex gap-4'>
                 <img src={assets.shuffle_icon} alt="" className="w-4 cursor-pointer"/>
-                <img src={assets.prev_icon} alt="" className="w-4 cursor-pointer"/>
+                <img onClick={previous} src={assets.prev_icon} alt="" className="w-4 cursor-pointer"/>
                 {playStatus 
                 ?<img onClick={pause} src={assets.pause_icon} alt="" className="w-4 cursor-pointer"/>
                 :<img onClick={play} src={assets.play_icon} alt="" className="w-4 cursor-pointer"/>
                 }                 
-                <img src={assets.next_icon} alt="" className="w-4 cursor-pointer"/>
+                <img onClick={next} src={assets.next_icon} alt="" className="w-4 cursor-pointer"/>
                 <img src={assets.loop_icon} alt="" className="w-4 cursor-pointer"/>
             </div>
             <div className='flex items-center gap-5'>
                 <p>{time.currentTime.minute}:{time.currentTime.second}</p>
-                <div ref={seekBg} className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
+                <div ref={seekBg} onClick={seekSong} className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
                     <hr ref={seekBar} className="h-1 border-none w-10 bg-green-800 rounded-full"/>
                 </div>
                 <p>{time.totalTime.minute}:{time.totalTime.second}</p>
